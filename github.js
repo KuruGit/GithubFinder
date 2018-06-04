0e1d5d6aa1756c6af7a9a79cfc2a0fe7b2ad98ef
David <david.breier@outlook.de>
2018-06-04 19:49:18 +0200

    fixed Error
    latest Repos were not displayed, fixed it (forEach function had no parameter übergeben

--- github.js
+++ github.js
@@ -2,12 +2,19 @@ class GitHub{
     constructor() {
         this.client_id ="734d43764055afe9f4ef";
         this.client_secret="b5187e06403976073a6296dfa9df829c53b816b0";
+        this.repos_count= 5;
+        this.repos_sort="created: asc";
     }
     async getUser(user){
         const profileResponse = await fetch (`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        const repoResponse = await fetch (`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}
+        &client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
         return {
-            profile
+            profile,
+            repos
         }
     }
 }
--- ui.js
+++ ui.js
@@ -36,7 +36,26 @@ class UI {
 
 
     }
+    
+    showRepos(repos){
+        let output="";
+        repos.forEach(function (repo) {
+            output += 
+            `
+            <div class="card card-body mb-2"></div>
+                <div class="row"></div>
+                    <div class="col-md-6"><a href="${repo.html_url}" target="_blank">${repo.name}</a></div> 
+                    <div class="col-md-6">
+                    <span class="badge badge-primary">Stars:${repo.stargazers_count}</span>
+        <span class="badge badge-secondary">Watchers:${repo.watchers_count}</span>
+        <span class="badge badge-success">Forks:${repo.forks_counts}</span>
+                    </div> 
 
+            `;
+                });
+    document.getElementById("repos").innerHTML=output;
+
+    }
     showAlert(message, className) {
         //clear remaining alerts
         this.clearAlert();
